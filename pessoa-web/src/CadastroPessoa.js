78b131cee0393e2b2b0a31a7d7b960036ee4d0dc
DAILAN BUENO DOS SANTOS <x04501348143@tcu.gov.br>
2019-03-26 16:01:51 -0300

    Ajuste formulario de cadastro de pessoa

--- pessoa-web/src/CadastroPessoa.js
+++ pessoa-web/src/CadastroPessoa.js
@@ -1,13 +1,20 @@
 import React, { Component } from 'react';
-import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
-import Card from '@material-ui/core/Card';
-import CardContent from '@material-ui/core/CardContent';
+import PropTypes from 'prop-types';
 import Typography from '@material-ui/core/Typography';
 import Grid from '@material-ui/core/Grid';
 import TextField from '@material-ui/core/TextField';
+import SelecionaSexo from './SelecionaSexo';
+import Card from '@material-ui/core/Card';
+import CardActions from '@material-ui/core/CardActions';
+
+
 
 const styles = {
+  root: {
+    flexGrow: 1,
+  },
+
   card: {
     minWidth: 250,
   },
@@ -26,34 +33,62 @@ const styles = {
 
 class CadastroPessoa extends Component {
   render() {
-    const {classes}  = this.props
-    return (
-      <div style={{width: '80%'}}>
-      <Card justify='center'>
-        <CardContent className={classes.card}>
-          <Typography variant="h5" component="h2">
-            Incluir pessoa
-          </Typography>
-           <br/>
-          <Grid container spacing={24}> 
-            <Grid item xs={12}>
-               <Grid item xs={6}>
-                  <TextField
-                    id="outlined-full-width"
-                    label="Nome completo"
-                    style={{ margin: 10, with: 50 }}
-                    placeholder="Entre com o nome completo"
-                    fullWidth
-                    margin="normal"
-                    variant="outlined"/>
-                </Grid>
-            </Grid>
-
-            <Grid item xs={6}>
+    return ( 
+      <div>
+      <Card>
+        <Typography  variant="subtitle1" gutterBottom>
+            Dados pessoas
+        </Typography>
+        <CardActions>
+          <Grid container spacing={24}>
+            <Grid item xs={12}>  
               <TextField
+                  id="outlined-full-width"
+                  label="CPF"
+                  style={{ margin: 10, width:400}}
+                  placeholder="Digite seu cpf"  
+                  fullWidth
+                  margin="normal"
+                  variant="outlined"/>
+            </Grid>
+            <Grid item xs={8}>
+                <TextField
+                  id="outlined-full-width"
+                  label="Nome completo"
+                  style={{ margin: 10}}
+                  placeholder="Entre com o nome completo"
+                  fullWidth
+                  margin="normal"
+                  variant="outlined"/>
+            </Grid>
+            <Grid item xs={4}>
+              <SelecionaSexo/>
+            </Grid>
+            <Grid item xs={3}>
+                <TextField
+                  id="outlined-full-width"
+                  label="Celular"
+                  style={{ margin: 10}}
+                  placeholder="Entre com o nome celular"
+                  fullWidth
+                  margin="normal"
+                  variant="outlined"/>
+            </Grid>
+            <Grid item xs={3}>
+              <TextField
+                  id="outlined-full-width"
+                  label="Telefone"
+                  style={{ margin: 10}}
+                  placeholder="Entre com o nome telefone"
+                  fullWidth
+                  margin="normal"
+                  variant="outlined"/>
+            </Grid>
+            <Grid item xs={6}>
+                <TextField
                   id="outlined-email-input"
                   label="Email"
-                  style={{ margin: 8 }}
+                  style={{ margin: 10 }}
                   type="email"
                   name="email"
                   autoComplete="email"
@@ -62,10 +97,12 @@ class CadastroPessoa extends Component {
                   fullWidth
                   variant="outlined"/>
             </Grid>
-        </Grid>
-        </CardContent>
+          
+          </Grid>
+        </CardActions>
+
       </Card>
-      </div>
+    </div>
     );
   }
 }
